fb355286aa61790fde7e84155707ef3d7f65b563
thiagoTL <thiago.lourenco@deway.com.br>
2020-08-27 10:28:24 -0300

    Produto

--- src/pages/produto.js
+++ src/pages/produto.js
@@ -1,34 +1,43 @@
 import React, { Fragment, Component } from "react";
-import { StyleSheet, Text , View ,ScrollView, FlatList, Button, TextInput,TouchableOpacity } from 'react-native';
-import Modal from 'react-native-modal'
-import Header from '../components/cabecalho/cabecalho'
-import Icon from 'react-native-vector-icons/FontAwesome';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  FlatList,
+  Button,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import Modal from "react-native-modal";
+import Header from "../components/cabecalho/cabecalho";
+import Icon from "react-native-vector-icons/FontAwesome";
 
-
-class Produto extends Component{
+class Produto extends Component {
   state = {
     personagens: {
-      id:0,
-      nome:'',
-      valor:0.0,
-      quantidade:0
+      id: 0,
+      nome: "",
+      valor: 0.0,
+      quantidade: 0,
     },
-    isModalVisible:false
-  }
+    isModalVisible: false,
+  };
 
-
-    openModal = () =>{
-      this.setState({
-      isModalVisible:!this.state.isModalVisible
-      })
-      }
+  openModal = () => {
+    this.setState({
+      isModalVisible: !this.state.isModalVisible,
+    });
+  };
   async componentWillMount() {
     try {
-      const response = await fetch('https://organizadbapi.herokuapp.com/v1/produto');
+      const response = await fetch(
+        "https://organizadbapi.herokuapp.com/v1/produto"
+      );
       const dataJson = await response.json();
 
       this.setState({ personagens: dataJson });
-      console.log(this.state.personagens)
+      console.log(this.state.personagens);
     } catch (error) {
       console.log(error);
     }
@@ -37,82 +46,86 @@ class Produto extends Component{
   render() {
     const { isModalVisible } = this.state;
 
-  return (
-    
-     <ScrollView>
-       <View style={styles.container}>
-        <View style={styles.header}>
-        <Header>
-          <Text style={styles.textHeader}>Js Organiza</Text>
-        </Header>
+    return (
+      <ScrollView>
+        <View style={styles.container}>
+          <View style={styles.header}>
+            <Header>
+              <Text style={styles.textHeader}>Js Organiza</Text>
+            </Header>
+          </View>
+          <FlatList
+            data={this.state.personagens}
+            renderItem={({ item }) => (
+              <View style={styles.cardItem}>
+                <Text>Id:{item.id}</Text>
+                <Text>Nome do Produto:{item.nome}</Text>
+                <Text>Valor:{item.valor}</Text>
+                <Text>Quantidade:{item.quantidade}</Text>
+                <Button
+                  title="Editar"
+                  type="outline"
+                  onPress={() => this.openModal()}
+                />
+                <Button title="Deletar" type="outline" />
+              </View>
+            )}
+          >
+            {" "}
+          </FlatList>
+          <Modal isVisible={this.state.isModalVisible}>
+            <View style={styles.contentModal}>
+              <View style={styles.viewForm}>
+                <Text style={styles.textLabel}>Nome: </Text>
+                <TextInput style={styles.textInput} placeholder="Name" />
+                <TouchableOpacity style={styles.btnEdit}>
+                  <Text style={styles.btnEditText}>Editar</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </Modal>
         </View>
-      <FlatList data={this.state.personagens} 
-          renderItem={({item})=>
-          <View style={styles.cardItem}>
-              <Text >Id:{item.id}</Text>
-              <Text >Nome do Produto:{item.nome}</Text>
-              <Text >Valor:{item.valor}</Text>
-              <Text >Quantidade:{item.quantidade}</Text>
-              <Button title="Editar" type="outline" onPress={()=>this.openModal()}/> 
-              <Button title="Deletar" type="outline"/> 
-         </View>
-  }> </FlatList>
-        <Modal isVisible={this.state.isModalVisible} >
-          <View style={styles.contentModal}>
-          <View style={styles.viewForm}>
-            <Text style={styles.textLabel}>Nome: </Text>
-            <TextInput style={styles.textInput} placeholder="Name"/>
-            <TouchableOpacity style={styles.btnEdit}>
-              <Text style={styles.btnEditText}>Editar</Text>
-            </TouchableOpacity>
-          </View>
-          </View>
-          
-        
-        </Modal>
-      </View>
-    </ScrollView>
-  );
-
-   }
+      </ScrollView>
+    );
+  }
 }
 
 export default Produto;
 
 const styles = StyleSheet.create({
-  container:{
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'stretch',
+  container: {
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "stretch",
   },
-  header:{
-      alignItems:"flex-start", 
-      height: 70, 
-      backgroundColor: 'powderblue',
-      justifyContent:"center",
-      padding:10
+  header: {
+    alignItems: "flex-start",
+    height: 70,
+    backgroundColor: "powderblue",
+    justifyContent: "center",
+    padding: 10,
   },
-  textHeader:{
-    color: 'white' , 
-    fontWeight: 'bold',
-    fontSize:30,
+  textHeader: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 30,
   },
-  cardItem:{
-      borderWidth: 1,
-      borderColor: "#20232a",
-      borderRadius: 6,
-      margin:10,
-      alignItems:"center",
-      textAlign: "center",
-      fontSize: 20,
-      padding:10
+  cardItem: {
+    borderWidth: 1,
+    borderColor: "#20232a",
+    borderRadius: 6,
+    margin: 10,
+    alignItems: "center",
+    textAlign: "center",
+    fontSize: 20,
+    padding: 10,
   },
   contentModal: {
     height: 300,
     width: "85%",
     borderRadius: 20,
     backgroundColor: "#fff",
-    alignSelf: "center"
+    alignSelf: "center",
   },
   viewForm: {
     marginTop: 10,
@@ -121,9 +134,8 @@ const styles = StyleSheet.create({
   textLabel: {
     fontSize: 16,
     color: "#212121",
-    fontWeight:"bold",
-    marginLeft: 20
-
+    fontWeight: "bold",
+    marginLeft: 20,
   },
   textInput: {
     height: 40,
@@ -133,7 +145,7 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     marginTop: 10,
     paddingLeft: 10,
-    marginLeft: 20
+    marginLeft: 20,
   },
   btnEdit: {
     height: 40,
@@ -143,11 +155,10 @@ const styles = StyleSheet.create({
     alignItems: "center",
     borderRadius: 5,
     marginTop: 50,
-    alignSelf: "center"
+    alignSelf: "center",
   },
   btnEditText: {
     color: "#fff",
-    fontSize: 14
-  }
+    fontSize: 14,
+  },
 });
-
